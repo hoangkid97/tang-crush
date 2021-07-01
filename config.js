f2ddc778d6b141a8bf5219672a1fe0c844e5af7b
hoangkid97 <86759370+hoangkid97@users.noreply.github.com>
2021-07-01 11:23:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'yêu Anh hônggg',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Xuân Quàng yêu Ngọc Ánhhhh <33',
-    btnNo: 'Yêu rất nhiềuuuuu :3',
+    btnYes: 'yêu Xuân Quàng nhìuuu <33',
+    btnNo: 'Anh là cái đồ đáng ghét :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho Ngọc Ánhh <3',
+    btnReply: 'Gửi cho Xuân Quàng đẹp trai <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Ngọc Ánh biết mà 🥰. Yêu Xuân Hoàng nhiều nhiều 😘😘',
-    messDesc: 'Chừng nào hết dịch nhớ đưa e đi khắp mọi nơi nha Aiuuuuu <3.',
-    btnAccept: 'Okiii, tuân lệnhhhh <3',
+    mess: 'Xuân Quàng biết mà 🥰. Yêu Ngọc Ánh nhiều nhiều 😘😘',
+    messDesc: 'Chừng nào hết dịch ck đưa dợ iuu đi khắp mọi nơi nha <3.',
+    btnAccept: 'Okiii, Ông chã của emmm <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
